4a4e7827b7df17a82cb9058c578c3185a9f96316
Vivek R <vividvilla@gmail.com>
2016-12-10 12:06:06 +0530

    Bumped version and updated setup file with README description

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
-
+from codecs import open
 from setuptools import setup
+
+README = open("README.md").read()
 
 setup(
 	name="rbiparser",
-	version="0.03",
+	version="0.5",
 	description="A utility for downloading, parsing and sanitizing bank database (IFSC, MICR, address etc.) Excel sheets from the RBI website.",
+	long_description=README,
 	author="Kailash Nadh",
 	author_email="kailash@nadh.in",
 	url="https://github.com/knadh/rbiparser",
